6e7e84383ee75c54de85750c2de1b67a094310cb
anaiancu24 <ana.iulia24@icloud.com>
2018-11-20 12:09:29 +0100

    trying

---
+++ src/pages/controller.ts
@@ -0,0 +1,23 @@
+import { JsonController, Get, Param } from 'routing-controllers'
+import {pagesById}  from './data'
+
+import {Page}  from './data'
+
+type PageList = { pages: Page[] }
+
+@JsonController()
+export default class PageController {
+
+    @Get('/pages/:id')
+    getPage(
+        @Param('id') id: number
+    ): Page {
+        return pagesById[id]
+    }
+
+    @Get('/pages')
+    allPages(): PageList {
+        return { pages: Object.values(pagesById)}
+    }
+
+}
